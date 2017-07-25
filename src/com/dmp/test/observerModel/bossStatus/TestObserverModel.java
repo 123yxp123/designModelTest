876eb13278154391d268777eadcab61f5923ff48
843615848 <843615848@qq.com>
2017-07-25 16:03:09 +0800

    test changes

--- src/com/dmp/test/observerModel/bossStatus/TestObserverModel.java
+++ src/com/dmp/test/observerModel/bossStatus/TestObserverModel.java
@@ -11,14 +11,14 @@ public class TestObserverModel {
 	}
 	public static void test1(){
 		Subject nba_sub = new NBASubject();
-		nba_sub.attach(new NBAObserver("中国队", nba_sub));
-		nba_sub.attach(new NBAObserver("荷兰队", nba_sub));
-		nba_sub.attach(new NBAObserver("切尔西队", nba_sub));
+		nba_sub.attach(new NBAObserver("中国队球迷", nba_sub));
+		nba_sub.attach(new NBAObserver("荷兰队球迷", nba_sub));
+		nba_sub.attach(new NBAObserver("切尔西队球迷", nba_sub));
 		nba_sub.setSubjectState("老板回来了！");
 		nba_sub.notifyInfo();
 		Subject stock_sub = new NBASubject();
-		stock_sub.attach(new StockObserver("陆金所", stock_sub));
-		stock_sub.attach(new StockObserver("蚂蚁金服", stock_sub));
+		stock_sub.attach(new StockObserver("微信理财会员", stock_sub));
+		stock_sub.attach(new StockObserver("蚂蚁金服会员", stock_sub));
 		stock_sub.setSubjectState("老板又回来了！");	
 		stock_sub.notifyInfo();
 	}
